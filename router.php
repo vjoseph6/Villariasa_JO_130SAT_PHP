bbe118aad188a425072865eb6b0cd71993fb866e
vjoseph6 <vjosephlhee@gmail.com>
2024-10-01 02:04:04 +0800

    0928_25_intro-to-forms-and-request-methods

--- router.php
+++ router.php
@@ -1,14 +1,5 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
 function routeToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
@@ -21,8 +12,13 @@ function routeToController($uri, $routes)
 function abort($code = 404)
 {
     http_response_code($code);
+
     require "views/{$code}.php";
+
     die();
 }
+
+$routes = require('routes.php');
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
