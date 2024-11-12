582805f6a9e2b13a26653d57f817337282405ee6
vjoseph6 <vjosephlhee@gmail.com>
2024-11-13 00:08:03 +0800

    1116_40_how-to-handle-passwords-for-the-rest-of-your-career

--- views/index.view.php
+++ views/index.view.php
@@ -3,8 +3,9 @@
 <?php require('partials/banner.php') ?>
 
 <main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <p>Hello. Welcome to the home page.</p>
-    </div>
+  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to the home page.</p>
+  </div>
 </main>
+
 <?php require('partials/footer.php') ?>
